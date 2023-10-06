e04feb9c8fc576beb955cc69113789c2005e779f
Starwolf16 <jmtarrant00@gmail.com>
2023-10-06 16:52:12 -0400

    Updates to main.py

--- Term_Project/main.py
+++ Term_Project/main.py
@@ -1,10 +1,15 @@
-import spacy 
-import torch.nn as nn
-import pandas as pd
 import matplotlib.pyplot as plt
+import pandas as pd
+import torch
 
+from spacy.lang.en import English
 from sys import exit
 from torch.cuda.amp import GradScaler
 
-dataset = pd.read_csv("emails.csv")
+nlp = English()
 
+dataset = pd.read_csv("emails.csv")
+print(dataset.columns)
+
+CORPUS = dataset.pop("text")
+print(CORPUS)
